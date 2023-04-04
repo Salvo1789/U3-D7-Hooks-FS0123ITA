2dbaf4ce82000ec2194fb6bbf5d04971a0458c76
Salvo1789 <salvo.mercurio89@gmail.com>
2023-04-04 17:17:45 +0200

    epibooks hooks

--- U3-D7-Epibooks-Recap-FS0123ITA-master/src/components/MyFooter.jsx
+++
@@ -1,9 +0,0 @@
-const MyFooter = () => (
-  <footer className="d-flex bg-dark">
-    <span className="text-white m-auto">
-      <strong>EPICODE</strong> - Copyright {new Date().getFullYear()}
-    </span>
-  </footer>
-)
-
-export default MyFooter
