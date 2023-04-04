fab0d2847c11a8d3323821bebe2f592db9b3a785
Salvo1789 <salvo.mercurio89@gmail.com>
2023-04-04 16:57:21 +0200

    Epibooks hooks

---
+++ U3-D7-Epibooks-Recap-FS0123ITA-master/src/components/AddComment.jsx
@@ -0,0 +1,78 @@
+import { Button, Form } from "react-bootstrap";
+import { useState, useEffect } from 'react';
+
+const AddComment = (props) => {
+  
+  const [comment, setComment] = useState("")
+  const [rate, setRate] = useState("1")
+  const [elementId, setElementId] = useState("")
+
+  useEffect(() => {
+    setElementId(props.asin)
+  }, [props.asin]
+  )
+
+  const sendComment = async e => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("https://striveschool-api.herokuapp.com/api/comments/", {
+        method: "POST",
+        body: JSON.stringify(comment, rate, elementId),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDJhZThlNTY4MzQzMTAwMTRkZWE3ZWYiLCJpYXQiOjE2ODA1MzM3MzMsImV4cCI6MTY4MTc0MzMzM30.BybWrL_zO0q23jXsrG8pA-yPWXt9QYBf4zZcRNL920U"
+        }
+      });
+      if (response.ok) {
+        // alert("Commento inviato");
+
+        // ricrea la lista di commenti nel livello superiore (il padre: CommentArea)
+        // attraverso la referenza della funzione fetchComments passata come prop a questo componente
+        props.fetchComments()
+
+        // resetta i campi (svuotarli)
+        setComment("")
+        setRate("")
+      }
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+    return (
+      <Form onSubmit={sendComment}>
+        <Form.Group className="mb-3" controlId="comment">
+          <Form.Label>Commento</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Inserisci il commento"
+            value={comment}
+            onChange={e => setComment(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="comment">
+          <Form.Label>Voto</Form.Label>
+          <Form.Select
+            value={rate}
+            onChange={e => {
+              setRate(e.target.value)
+            }}
+          >
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </Form.Select>
+        </Form.Group>
+
+        <Button type="submit" variant="primary">
+          Invia commento
+        </Button>
+      </Form>
+    );
+}
+
+export default AddComment;
